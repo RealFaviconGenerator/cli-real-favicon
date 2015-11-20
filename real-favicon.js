32e53564e7dba3f5dde0700b264acdfb153a656c
Philippe Bernard <philippe.bernard@gmail.com>
2015-11-20 22:23:33 +0100

    Scripts renamed

--- real-favicon.js
+++ real-favicon.js
@@ -8,6 +8,10 @@ var pack = require('./package.json');
 
 program
   .version(pack.version)
+  .description(
+    'A command-line interface for RealFaviconGenerator. ' +
+    'Instead of reading some boring documentation, visit ' +
+    'https://realfavicongenerator.net/ to generate exactly what you need.')
   .command('generate <faviconRequest> <faviconFile> <outputDir>',
     'Generate favicon images and HTML markups')
   .command('inject <faviconFile> <outputDir> <htmlFiles>',
